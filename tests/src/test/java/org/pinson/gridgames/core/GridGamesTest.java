f4598810d1122c486c435cd5864b95e18720d506
Julien Pinson <dinkertons@gmail.com>
2023-02-19 21:39:35 +0100

    Added unit testing library

---
+++ tests/src/test/java/org/pinson/gridgames/core/GridGamesTest.java
@@ -0,0 +1,14 @@
+package org.pinson.gridgames.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GridGamesTest {
+
+    @Test
+    public void create() {
+        assertEquals(2+2, 4);
+    }
+
+}
