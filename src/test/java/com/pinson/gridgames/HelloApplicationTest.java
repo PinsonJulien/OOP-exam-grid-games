cd0d56bdb93135595f323955d89af4371c90aa39
Julien Pinson <dinkertons@gmail.com>
2023-02-27 14:29:02 +0100

    Added junit

---
+++ src/test/java/com/pinson/gridgames/HelloApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pinson.gridgames;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloApplicationTest {
+
+    @Test
+    void test() {
+        assertEquals(4, 2+2);
+    }
+}
